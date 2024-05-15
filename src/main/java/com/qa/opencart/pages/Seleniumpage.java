0ba853a857613743b48d5f7e792880ceef51ee89
Muthu9293 <102504689+Muthu9293@users.noreply.github.com>
2024-05-15 06:22:37 +0530

    Create Seleniumpage.java

---
+++ src/main/java/com/qa/opencart/pages/Seleniumpage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.qa.opencart.constants.AppConstants;
+import com.qa.opencart.utils.ElementUtil;
+
+public class Seleniumpage {
+
+	private WebDriver driver;
+	private ElementUtil eleUtil;
+}
